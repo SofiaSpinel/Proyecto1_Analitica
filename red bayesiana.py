107089ce9cb51d96531c89681c136097893c52de
vidiazc <v.diazc@uniandes.edu.co>
2023-09-25 22:47:32 -0500

    agrupando variables en data y accuracy en red

--- red bayesiana.py
+++ red bayesiana.py
@@ -3,14 +3,14 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.sampling import BayesianModelSampling
 from pgmpy.estimators import MaximumLikelihoodEstimator
-import itertools
-from itertools import permutations
+
 
 #cargar dataframe
-df = pd.read_excel("C:/Users/valer/OneDrive/Documentos/Analitica de datos/Proyecto 1/data_variables.xlsx")
+df = pd.read_excel("C:/Users/valer/OneDrive/Documentos/GitHub/Proyecto1_Analitica/data_variables.xlsx")
+df.head()
 
 #crear modelo
-model = BayesianNetwork([('mquali','tuition'),('mocup','tuition'),('age','target'),('course','grade'),('mquali','mocup'),('grade','target'),('mocup','target'),('target','unrate')])
+model = BayesianNetwork([('tuition','target'),('age','target'),('course','grade'),('mquali','mocup'),('grade','target'),('mocup','target'),('target','unrate')])
 
 #maxima verosimilitud
 emv = MaximumLikelihoodEstimator(model= model, data=df)
@@ -27,7 +27,51 @@ model.fit(data=df, estimator = MaximumLikelihoodEstimator)
 
 for i in model.nodes():
     print(model.get_cpds(i))
+    
+import numpy
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
 
+X = df.drop('target', axis=1)  # Las características predictoras, excluyendo 'lung'
+y = df['target']  # La variable objetivo 'lung'
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Crear una instancia de LabelEncoder
+label_encoder = LabelEncoder()
+
+# Aplicar la codificación a las columnas categóricas
+categorical_columns = ['course', 'mquali', 'mocup', 'tuition', 'unrate', 'age', 'grade']
+for col in categorical_columns:
+    X_train[col] = label_encoder.fit_transform(X_train[col])
+    X_test[col] = label_encoder.transform(X_test[col])
+
+# Ahora puedes continuar con la creación y ajuste del modelo
+model = MLPClassifier(max_iter=1000, random_state=42)
+model.fit(X_train, y_train)
+
+# Crea y ajusta el modelo de red neuronal
+# Realiza predicciones en los datos de prueba
+y_pred = model.predict(X_test)
+
+# Calcula la exactitud del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Exactitud del modelo: {accuracy}')
+
+# Calcula y reporta Verdaderos Positivos, Falsos Positivos, Verdaderos Negativos y Falsos Negativos
+confusion = confusion_matrix(y_test, y_pred)
+true_positives = confusion[1, 1]
+false_positives = confusion[0, 1]
+true_negatives = confusion[0, 0]
+false_negatives = confusion[1, 0]
+
+print(f'Verdaderos Positivos: {true_positives}')
+print(f'Falsos Positivos: {false_positives}')
+print(f'Verdaderos Negativos: {true_negatives}')
+print(f'Falsos Negativos: {false_negatives}')
 
 
     
