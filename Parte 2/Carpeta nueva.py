9f489726529ba017baf21dd768e6b84032c400a1
SofiaSpinel <s.spinel@uniandes.edu.co>
2023-11-03 11:43:13 -0500

    Nuevos avances

--- Parte 2/Carpeta nueva.py
+++ Parte 2/Carpeta nueva.py
@@ -38,23 +38,24 @@ print(cpd_course_2)
 #estimar todo el modelo
 model_2.fit(data=df, estimator = MaximumLikelihoodEstimator)
 
+#Metodos aplicados en el modelo --------------------------------------------------------------------
 for i in model_2.nodes():
     print(model_2.get_cpds(i))
     
 scoring_method = K2Score(data=df)
 esth = HillClimbSearch(data=df)
 estimated_modelh = esth.estimate(
-    scoring_method=scoring_method, max_indegree=4, max_iter=int(1e4)
+    scoring_method=scoring_method, max_indegree=6, max_iter=int(1e4)
 )
 
 from pgmpy.estimators import BicScore
 scoring_method_bic = BicScore(data=df)
 esth_bic = HillClimbSearch(data=df)
 estimated_modelh_bic = esth_bic.estimate(
-    scoring_method=scoring_method_bic, max_indegree=4, max_iter=int(1e4)
+    scoring_method=scoring_method_bic, max_indegree=6, max_iter=int(1e4)
 )
 
-#Accuracy y verdaderos positivos 
+#Accuracy y verdaderos positivos -------------------------------------------------------------------
 
 X = df.drop('target', axis=1)  # Las características predictoras, excluyendo 'target'
 y = df['target']  # La variable objetivo 'target'
@@ -71,17 +72,24 @@ for col in categorical_columns:
     X_train[col] = label_encoder.fit_transform(X_train[col])
     X_test[col] = label_encoder.transform(X_test[col])
 
+#Evaluar modelo con K2-------------------------------------------------------------------------------
+print(estimated_modelh)
+print("--------------")
+print(estimated_modelh.nodes())
+print("--------------")
+print(estimated_modelh.edges())
 
+print(scoring_method.score(estimated_modelh))
 # Crea y ajusta el modelo de red neuronal
 # Realiza predicciones en los datos de prueba
-y_pred = model_2.predict(X_test)
+"""y_pred_K2 = estimated_modelh.predict(X_test)
 
 # Calcula la exactitud del modelo
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred_K2)
 print(f'Exactitud del modelo: {accuracy}')
 
 # Calcula y reporta Verdaderos Positivos, Falsos Positivos, Verdaderos Negativos y Falsos Negativos
-confusion = confusion_matrix(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred_K2)
 true_positives = confusion[1, 1]
 false_positives = confusion[0, 1]
 true_negatives = confusion[0, 0]
@@ -90,4 +98,102 @@ false_negatives = confusion[1, 0]
 print(f'Verdaderos Positivos: {true_positives}')
 print(f'Falsos Positivos: {false_positives}')
 print(f'Verdaderos Negativos: {true_negatives}')
-print(f'Falsos Negativos: {false_negatives}')
+print(f'Falsos Negativos: {false_negatives}')"""
+
+#Evaluar con Score-------------------------------------------------------------------------------------
+print(estimated_modelh_bic)
+print("--------------")
+print(estimated_modelh_bic.nodes())
+print("--------------")
+print(estimated_modelh_bic.edges())
+print("--------------")
+print(scoring_method.score(estimated_modelh_bic))
+# Crea y ajusta el modelo de red neuronal
+# Realiza predicciones en los datos de prueba
+"""y_pred_score = estimated_modelh_bic.predict(X_test)
+
+# Calcula la exactitud del modelo
+accuracy = accuracy_score(y_test, y_pred_score)
+print(f'Exactitud del modelo: {accuracy}')
+
+# Calcula y reporta Verdaderos Positivos, Falsos Positivos, Verdaderos Negativos y Falsos Negativos
+confusion = confusion_matrix(y_test, y_pred_score)
+true_positives = confusion[1, 1]
+false_positives = confusion[0, 1]
+true_negatives = confusion[0, 0]
+false_negatives = confusion[1, 0]
+
+print(f'Verdaderos Positivos: {true_positives}')
+print(f'Falsos Positivos: {false_positives}')
+print(f'Verdaderos Negativos: {true_negatives}')
+print(f'Falsos Negativos: {false_negatives}')"""
+
+#Hacer cambios?---------------------------------------------------------------------------------------
+for i in model_2.nodes():
+    print(model_2.get_cpds(i))
+    
+scoring_method = K2Score(data=df)
+esth = HillClimbSearch(data=df)
+estimated_modelh_2 = esth.estimate(
+    scoring_method=scoring_method, max_indegree=7, max_iter=int(1e4)
+)
+
+print("------k2--------")
+print(estimated_modelh_2)
+print("--------------")
+print(estimated_modelh_2.nodes())
+print("--------------")
+print(estimated_modelh_2.edges())
+
+print(scoring_method.score(estimated_modelh_2))
+
+from pgmpy.estimators import BicScore
+scoring_method_bic = BicScore(data=df)
+esth_bic = HillClimbSearch(data=df)
+estimated_modelh_bic_2 = esth_bic.estimate(
+    scoring_method=scoring_method_bic, max_indegree=7, max_iter=int(1e4)
+)
+
+print("------Score--------")
+print(estimated_modelh_bic_2)
+print("--------------")
+print(estimated_modelh_bic_2.nodes())
+print("--------------")
+print(estimated_modelh_bic_2.edges())
+print("--------------")
+print(scoring_method.score(estimated_modelh_bic_2))
+
+#Hacer mas pruebas---------------------------------------------------
+for i in model_2.nodes():
+    print(model_2.get_cpds(i))
+    
+scoring_method = K2Score(data=df)
+esth = HillClimbSearch(data=df)
+estimated_modelh_3 = esth.estimate(
+    scoring_method=scoring_method, max_indegree=4, max_iter=int(1e4)
+)
+
+print("------k2--------")
+print(estimated_modelh_3)
+print("--------------")
+print(estimated_modelh_3.nodes())
+print("--------------")
+print(estimated_modelh_3.edges())
+
+print(scoring_method.score(estimated_modelh_3))
+
+from pgmpy.estimators import BicScore
+scoring_method_bic = BicScore(data=df)
+esth_bic = HillClimbSearch(data=df)
+estimated_modelh_bic_3 = esth_bic.estimate(
+    scoring_method=scoring_method_bic, max_indegree=4, max_iter=int(1e4)
+)
+
+print("------Score--------")
+print(estimated_modelh_bic_3)
+print("--------------")
+print(estimated_modelh_bic_3.nodes())
+print("--------------")
+print(estimated_modelh_bic_3.edges())
+print("--------------")
+print(scoring_method.score(estimated_modelh_bic_3))
