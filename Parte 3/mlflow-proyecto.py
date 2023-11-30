65ba39e67d038a386d95cce4a436226eff15e983
SofiaSpinel <s.spinel@uniandes.edu.co>
2023-11-30 16:03:26 -0500

    otros cambios

--- Parte 3/mlflow-proyecto.py
+++ Parte 3/mlflow-proyecto.py
@@ -11,8 +11,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y)
 #Importe MLFlow para registrar los experimentos, el regresor de bosques aleatorios y la métrica de error cuadrático medio
 import mlflow
 import mlflow.sklearn
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+#from sklearn.ensemble import RandomForestRegressor
+#from sklearn.metrics import mean_squared_error
+from pgmpy.models import BayesianNetwork
+from pgmpy.estimators import MaximumLikelihoodEstimator
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # defina el servidor para llevar el registro de modelos y artefactos
 # mlflow.set_tracking_uri('http://0.0.0.0:5000')
@@ -27,10 +30,19 @@ with mlflow.start_run(experiment_id=experiment.experiment_id):
     max_depth = 6
     max_features = 3
     # Cree el modelo con los par�metros definidos y entrénelo
-    rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
-    rf.fit(X_train, y_train)
+    #rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
+    #rf.fit(X_train, y_train)
+    modelo = BayesianNetwork([('COLE_BILINGUE', 'puntaje'),
+                              ('COLE_JORNADA','puntaje'),
+                              ('COLE_NATURALEZA','puntaje'),
+                              ('ESTU_GENERO','puntaje'),
+                              ('FAMI_ESTRATOVIVIENDA','puntaje'),
+                              ('FAMI_TIENEINTERNET','puntaje'),
+                              ('FAMI_TIENECOMPUTADOR','puntaje'),])
+    emv = MaximumLikelihoodEstimator(model= modelo, data=db)
+    modelo.fit(data=db, estimator = MaximumLikelihoodEstimator)
     # Realice predicciones de prueba
-    predictions = rf.predict(X_test)
+    y_pred = modelo.predict(X_test)
   
     # Registre los par�metros
     mlflow.log_param("num_trees", n_estimators)
@@ -38,9 +50,12 @@ with mlflow.start_run(experiment_id=experiment.experiment_id):
     mlflow.log_param("max_feat", max_features)
   
     # Registre el modelo
-    mlflow.sklearn.log_model(rf, "random-forest-model")
+    mlflow.sklearn.log_model(modelo, "random-forest-model")
   
     # Cree y registre la mtrica de interés
-    mse = mean_squared_error(y_test, predictions)
-    mlflow.log_metric("mse", mse)
-    print(mse)
+    #mse = mean_squared_error(y_test, predictions)
+    #mlflow.log_metric("mse", mse)
+    #print(mse)# Calcula la exactitud del modelo
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'Exactitud del modelo: {accuracy}')
+    
